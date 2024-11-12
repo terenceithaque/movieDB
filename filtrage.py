737a78b30d5800b7ca669ea01b400149f09f9571
Terence Le Thierry <terence.lethierry@protonmail.com>
2024-11-12 19:07:12 +0100

    La fonction filtrer peut procéder au filtrage avec ou sans respect de la casse

--- filtrage.py
+++ filtrage.py
@@ -1,18 +1,23 @@
 "filtrage.py gère les opération de filtrage de recherches"
-from imdb import *
+#from imdb import *
 
-def filtrer(liste:list, filtre:str) -> list:
+def filtrer(liste:list, filtre:str, respect_casse=False) -> list:
     """Renvoie une copie de liste filtrée avec filtre. \n
-        Exemple d'utilisation:
+        Exemple d'utilisation:\n
         ma_liste = ["Hello", "world", "!"] \n
-        print(filtrer(ma_liste, "Hello"))
+        print(filtrer(ma_liste, "Hello")) \n
         ['Hello']
         """
     liste_filtree = [] # Liste filtrée, vide pour l'instant
 
     for element in liste: # Pour chaque élément de la liste non filtrée
-        if filtre in element or element == filtre: # Si le filtre est compris dans l'élément ou que l'élément lui-même est le filtre
-            liste_filtree.append(element) # Ajouter l'élément à la liste filtrée
+        if respect_casse: # Si on doit respecter la casse
+            if str(filtre).lower() in str(element).lower() or str(element).lower() == str(filtre).lower(): # Si le filtre est compris dans l'élément ou que l'élément lui-même est le filtre
+                liste_filtree.append(element) # Ajouter l'élément à la liste filtrée
+
+        else: # Si on ne doit pas tenir compte de la casse
+            if str(filtre).lower() in str(element) or str(filtre).upper() in str(element) or element == filtre: 
+                liste_filtree.append(element)
 
 
     return liste_filtree        
