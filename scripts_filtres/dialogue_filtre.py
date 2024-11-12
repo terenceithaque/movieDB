415ba6dfc38e905727dc05089655ed3ee551dca2
Terence Le Thierry <terence.lethierry@protonmail.com>
2024-11-12 19:16:14 +0100

    Ajouté dialogue_filtre.py

---
+++ scripts_filtres/dialogue_filtre.py
@@ -0,0 +1,3 @@
+"dialogue_filtre.py contient une classe DialogueFiltre qui représente une boîte de dialogue dans laquelle l'utilisateur peut saisir un filtre"
+from tkinter import * # Importer tkinter
+
