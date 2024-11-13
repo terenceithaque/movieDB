efc59729fe009e46d57dd00ad79add55267fac2b
Terence Le Thierry <terence.lethierry@protonmail.com>
2024-11-13 13:03:22 +0100

    Ajouté des options de filtrage des résultats

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 from imdb import Cinemagoer # Importer Cinemagoer depuis l'API imdb
 from recherche import * # Importer le module recherche qui contient les fonctions de recherche sur IMDB
 from scripts_filtres.filtrage import * # Importer le module filtrage, qui contient des fonctions pour appliquer des filtres de recherche, depuis le dossier scripts_filtres
-
+from scripts_filtres.dialogue_filtre import * # Importer le module dialogue_filtre depuis le dossier scripts_filtres afin de pouvoir demander à l'utilisateur de saisir des filtres
 import threading
 
 class Application(Tk):
@@ -24,9 +24,9 @@ class Application(Tk):
         self.menu_tri_filtres.add_cascade(label="Tri...", menu=self.sous_menu_tri)
         self.sous_menu_filtres = Menu(self.menu_tri_filtres, tearoff=0) # Menu pour les options de filtre
 
-        self.sous_menu_filtres.add_command(label="Titre...", command=None) # Commande de filtrage par titre
-        self.sous_menu_filtres.add_command(label="Réalisateur(s)...", command=None) # Commande de filtrage par réalisateur(s)
-        self.sous_menu_filtres.add_command(label="Année...", command=None) # Commande de filtrage par année de sortie
+        self.sous_menu_filtres.add_command(label="Titre...", command=lambda:self.filtrer_resultats(type_filtre="titre")) # Commande de filtrage par titre
+        self.sous_menu_filtres.add_command(label="Réalisateur(s)...", command=lambda:self.filtrer_resultats(type_filtre="réalisateur(s)")) # Commande de filtrage par réalisateur(s)
+        self.sous_menu_filtres.add_command(label="Année...", command=lambda:self.filtrer_resultats(type_filtre="année")) # Commande de filtrage par année de sortie
 
         self.menu_tri_filtres.add_cascade(label="Filtres...", menu=self.sous_menu_filtres)
 
@@ -47,9 +47,11 @@ class Application(Tk):
         self.bouton_rechercher.pack()                                                                                                                  
 
 
-        self.label_resultats_affiches = Label(self, text="Aucune recherche") # Libellé indiquant le nombre de résultats affichés et le nombre de résultats trouvés
-        self.label_resultats_affiches.pack()
+        self.label_statut_recherche = Label(self, text="Aucune recherche") # Libellé indiquant le statut de la recherche
+        self.label_statut_recherche.pack()
         self.resultats_affiches = 0 # Nombre de résultats affichés dans l'arbre visuel
+
+        self.resultats_recherche = [] # Liste des résultats de recherche, vide pour l'instant
 
         # Définir les colonnes pour l'arbre visuel des résultats de recherche
         self.colonnes = ("titre", "réalisateur", "année")
@@ -69,21 +71,23 @@ class Application(Tk):
         #self.arbre_resultats.delete("1.0", END)
         # Effacer les résultats de recherche précédents
 
+
+        self.label_statut_recherche.config(text="Recherche en cours. Cela peut prendre un peu de temps...")
         
 
         for resultat in self.arbre_resultats.get_children():
             self.arbre_resultats.delete(resultat)
             self.resultats_affiches -= 1 # Réduire le nombre résultats affichés
 
-        self.arbre_resultats.insert("", END, values="".join("Recherhe en cours..."))
+        self.arbre_resultats.insert("", END, values="".join("Recherche en cours..."))
         self.arbre_resultats.update() # Mettre à jour l'arbre pour afficher le texte
-        resultats_recheche = rechercher(self.barre_recherche.get()) # Rechercher dans IMDB les éléments qui correspondent à la requête entrée dans la barre de recherche
+        self.resultats_recheche = rechercher(self.barre_recherche.get()) # Rechercher dans IMDB les éléments qui correspondent à la requête entrée dans la barre de recherche
         items = self.arbre_resultats.get_children() # Obtenir les items présents dans l'arbre visuel
         for item in items: # Supprimer les items
             self.arbre_resultats.delete(item)
         
-        if len(resultats_recheche) > 0: # Si des résultats de recherche ont été trouvés 
-            for resultat in resultats_recheche: # Pour chaque résultat de recherche
+        if len(self.resultats_recheche) > 0: # Si des résultats de recherche ont été trouvés 
+            for resultat in self.resultats_recheche: # Pour chaque résultat de recherche
                 self.arbre_resultats.insert("", END, values=resultat) # Ajouter le résultat à l'arbre de résultats
                 self.resultats_affiches += 1 # Augmenter le nombre de résultats affichés
 
@@ -91,9 +95,36 @@ class Application(Tk):
             self.arbre_resultats.insert(parent="", index="end", text="Oups ! Pas de résultats de recherche.")
 
 
-        self.label_resultats_affiches.config(text=f"{self.resultats_affiches} résultats affichés sur {len(resultats_recheche)}")    
+        self.label_statut_recherche.config(text=f"{self.resultats_affiches} résultats affichés sur {len(self.resultats_recheche)}")    
             
 
+
+    def filtrer_resultats(self, type_filtre=None | str):
+        "Demande un filtre à l'utilisateur et effectue un filtrage des données affichées dans l'arbre visuel en fonction de la saisie"
+        if self.resultats_recheche: # Si des résultats de recherche ont été trouvés
+            if not type_filtre: # Si le type du filtre n'est pas précisé
+                filtre = demander_filtre(type="Filtre", texte=f"Saisissez un filtre")
+
+            else: # Sinon
+                filtre = demander_filtre(type= f"Filtre de type {type_filtre}", texte=f"Saisissez un filtre de type {type_filtre}...")
+
+
+            resultats_filtres = filtrer(self.resultats_recheche, filtre) # Filtrer les résultats de recherche avec le filtre saisi
+            for resultat in self.arbre_resultats.get_children(): # Pour chaque résultat présent dans l'arbre visuel
+                if not resultat in resultats_filtres: # Si le résultat ne fait pas partie des résultats filtrés
+                    self.arbre_resultats.delete(resultat)   
+                    self.resultats_affiches -= 1
+                    self.label_statut_recherche.config(text=f"{self.resultats_affiches} résultats affichés sur {len(self.resultats_recheche)}")
+
+
+            for resultat in resultats_filtres: # Pour chaque résultat correspondant au filtre
+                self.arbre_resultats.insert("", END, values=resultat) # Ajouter le résultat à l'arbre visuel  
+                self.resultats_affiches += 1
+
+                
+
+        self.label_statut_recherche.config(text=f"{self.resultats_affiches} résultats affichés sur {len(self.resultats_recheche)}")
+        self.arbre_resultats.update() # Mettre à jour l'arbre visuel            
 
 
 app = Application() # Créer une instance de l'application
--- scripts_filtres/dialogue_filtre.py
+++ scripts_filtres/dialogue_filtre.py
@@ -1,3 +1,10 @@
-"dialogue_filtre.py contient une classe DialogueFiltre qui représente une boîte de dialogue dans laquelle l'utilisateur peut saisir un filtre"
-from tkinter import * # Importer tkinter
+"dialogue_filtre.py contient une fonction demander_filtre qui demande à l'utilisateur de saisir un filtre"
+from tkinter import simpledialog # Importer simpledialog depuis tkinter
 
+def demander_filtre(type="Filtre", texte="Saisissez un filtre:"):
+    """Demande un filtre à l'utilisateur avec texte comme demande. Si acun filtre n'est entré, renvoie une chaîne de caractères vide."""
+    filtre = simpledialog.askstring(title=type, prompt=texte) # Demander un filtre à l'utilisateur et en renvoyer la valeur
+    if not filtre: # Si l'utilisateur n'a pas saisi de filtre
+        filtre = "" # Alors on considère que le filtre est une chaîne de caractères vide
+
+    return filtre    
